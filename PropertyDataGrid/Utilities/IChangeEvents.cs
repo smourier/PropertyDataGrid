53b1f7004d69fd86bb18435b53558eda10122803
Simon Mourier <simon_mourier@hotmail.com>
2018-04-11 00:01:55 +0200

    Work in progress.

---
+++ PropertyDataGrid/Utilities/IChangeEvents.cs
@@ -0,0 +1,9 @@
+﻿namespace PropertyDataGrid.Utilities
+{
+    public interface IChangeEvents
+    {
+        bool RaiseOnPropertyChanging { get; set; }
+        bool RaiseOnPropertyChanged { get; set; }
+        bool RaiseOnErrorsChanged { get; set; }
+    }
+}
---
+++ PropertyDataGrid/Utilities/IChangeTrackingDictionaryObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Concurrent;
+
+namespace PropertyDataGrid.Utilities
+{
+    public interface IChangeTrackingDictionaryObject : IDictionaryObject
+    {
+        ConcurrentDictionary<string, DictionaryObjectProperty> ChangedProperties { get; }
+
+        void CommitChanges();
+        void RollbackChanges(DictionaryObjectPropertySetOptions options);
+    }
+}
